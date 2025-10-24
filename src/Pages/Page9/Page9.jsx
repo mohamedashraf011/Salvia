a3b547561e3fb5116ef1799ccd5fffd4d60222aa
Mohamed Ashraf <mohamed21-01148@student.eelu.edu.eg>
2025-10-24 18:54:37 +0300

    Refactor Page9 and Page10 components: enhance layout and styling, update heading font weight, and improve padding

--- src/Pages/Page9/Page9.jsx
+++ src/Pages/Page9/Page9.jsx
@@ -23,13 +23,13 @@ function Page9() {
       />
       
       <div className="flex flex-1 overflow-hidden">
-        <div className="w-1/2 flex flex-col px-4 py-6 overflow-y-auto scrollbar-hide">
-          <h1 className="text-4xl font-bold mb-8 flex-shrink-0">
+        <div className="w-1/2 flex flex-col px-10 py-10 overflow-y-auto scrollbar-hide">
+          <h1 className="text-4xl font-extrabold mb-8 flex-shrink-0">
             Heading
           </h1>
           <div className="flex-1 text-white text-lg leading-relaxed">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odio dicta consequatur sed ducimus illo minus amet odit et dolores! Animi voluptatibus neque quam doloribus aut laboriosam minus totam sequi.
             </p>
             <p className="mt-4">
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
@@ -37,7 +37,6 @@ function Page9() {
             <p className="mt-4">
               Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.
             </p>
-            {/* Add more paragraphs if needed to make it scrollable */}
             <p className="mt-4">
               At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
             </p>
@@ -47,7 +46,7 @@ function Page9() {
           </div>
         </div>
 
-        <div className="w-1/2 flex items-end justify-center p-4 pb-16">
+        <div className="w-1/2 flex items-end justify-center">
           <img
             src={rightImage}
             alt="Right Image"
